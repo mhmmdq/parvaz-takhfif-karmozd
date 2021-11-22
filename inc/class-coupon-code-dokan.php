39bfc4eaee7b144540617cbe8f6ad3bb9fef0ee4
mhmmdq <mhmmdq@mhmmdq.ir>
2021-11-22 18:47:03 +0330

    first commit

---
+++ inc/class-coupon-code-dokan.php
@@ -0,0 +1,10 @@
+<?php
+
+if( !class_exists( 'PARVAZ_CCD' ) )
+{
+    
+    class PARVAZ_CCD {
+        # I will make this plugin more professional in the near future
+    }
+
+}
---
+++ parvaz-takhfif-karmozd.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: پرواز - افزونه تخفیف از روی کارمزد
+Plugin URI: https://github.com/mhmmdq/parvaz-takhfif-karmozd
+Description: افزونه ای برای کسر کد تخفیف از کمیسیون مدیر به جای مبلغ کل
+Author: Mhmmdq
+Version: 0.1.0
+Author URI: https://github.com/mhmmdq
+*/
+if (!defined('ABSPATH'))
+  exit;
+
+add_action( 'woocommerce_before_calculate_totals', 'adjust_cart_coupon', 10, 1);
+
+function adjust_cart_coupon( $cart_object ) {
+    global $woocommerce;
+
+    if ( is_admin() && ! defined( 'DOING_AJAX' ) ){
+      return;
+    }
+
+    if ($coupons = WC()->cart->get_applied_coupons()  == False ) {
+        $coupon = False;
+    } else {
+      foreach ( WC()->cart->get_applied_coupons() as $code ) {
+        
+          $coupon = new WC_Coupon( $code );
+          $discount_type = $coupon->get_discount_type(); // Get coupon discount type
+          $coupon_amount = $coupon->get_amount(); // Get coupon amount
+          
+      }
+    }
+
+    if($coupon !== false) {
+      foreach ( $cart_object->get_cart() as $cart_item )
+      {
+        $commission =  dokan()->commission->get_earning_by_product( $cart_item['product_id'], 'admin' );
+        $price = $cart_item['data']->regular_price;
+        if($discount_type == 'percent') {
+          $price = $price - (($commission * $coupon_amount)/100);
+        }else {
+          $price = $price - ($commission - $coupon_amount);
+        }
+        $cart_item['data']->set_price( $price );
+      }
+
+    }
+
+  }
